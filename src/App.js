90cdc4db5222e601b7a30d4b41fabbbd9fd0576a
Anubhav Dhawan <anubhav.dhawan@ril.com>
2020-10-13 15:33:40 +0530

    add pdf renderer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Camera, { FACING_MODES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
+import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 
 function App (props) {
   function handleTakePhoto (dataUri) {
@@ -9,11 +10,43 @@ function App (props) {
   }
 
   return (
+    <>
     <Camera
       onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
       idealFacingMode={FACING_MODES.ENVIRONMENT}
     />
+      <PDFViewer>
+      <MyDocument />
+    </PDFViewer>
+    </>
   );
 }
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
 
 export default App;
