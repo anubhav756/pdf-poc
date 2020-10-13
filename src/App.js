04db4244a982fc959b9f7e665f03c82d870f5518
Anubhav Dhawan <anubhav.dhawan@ril.com>
2020-10-13 17:52:31 +0530

    pdf preview update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import Camera, { FACING_MODES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { jsPDF } from "jspdf";
+import { jsPDF } from 'jspdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -10,25 +10,34 @@ const App = () => {
   const doc = useRef(new jsPDF());
   const [pages, setPages] = useState(0);
   const handleTakePhoto = (dataUri) => {
+    if (pages) doc.current.addPage();
+
     doc.current.addImage(
       dataUri,
       'PNG',
       0,
-      pages * 100,
       0,
-      100,
     );
+  
     setPages(pages + 1);
   }
 
   return (
     <>
     <Camera
-      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+      onTakePhoto = {handleTakePhoto}
       idealFacingMode={FACING_MODES.ENVIRONMENT}
     />
     <Document file={doc.current.output('blob')}>
-      <Page pageNumber={1} />
+      {Array.from(
+        new Array(doc.current.getNumberOfPages()),
+        (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+          />
+        ),
+      )}
     </Document>
     </>
   );
