85d85ddb68aaed972e455863794e33f24588d068
Anubhav Dhawan <anubhav.dhawan@ril.com>
2020-10-14 15:55:50 +0530

    update ideal resolution

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Camera, { FACING_MODES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
-import { Document, Page, pdfjs } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
 import { jsPDF } from 'jspdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const App = () => {
-  const doc = useRef(new jsPDF());
-  const [pages, setPages] = useState(0);
-  const handleTakePhoto = (dataUri) => {
-    if (pages) doc.current.addPage();
+  const [images, setImages] = useState([]);
+  const handleTakePhoto = (dataUri) => setImages([...images, dataUri]);
+  const cameraRef = useRef();
+  const handleSubmit = () => {
+    const { clientWidth, clientHeight } = cameraRef.current;
+    const doc = new jsPDF({
+      unit: 'px',
+      format: [clientWidth, clientHeight],
+    });
 
-    doc.current.addImage(
-      dataUri,
-      'PNG',
-      0,
-      0,
-    );
+    images.forEach((image, i) => {
+      if (i > 0) doc.addPage();
 
-    if (pages === 30)
-      doc.current.save();
-  
-    setPages(pages + 1);
-  }
+      doc.addImage(image, 'png', 0, 0, clientWidth, clientHeight);
+    });
+
+    doc.save();
+  };
 
   return (
-    <>
-    <Camera
-      onTakePhoto = {handleTakePhoto}
-      idealFacingMode={FACING_MODES.ENVIRONMENT}
-    />
-    <Document file={doc.current.output('blob')}>
-      {Array.from(
-        new Array(doc.current.getNumberOfPages()),
-        (el, index) => (
-          <Page
-            key={`page_${index + 1}`}
-            pageNumber={index + 1}
-          />
-        ),
-      )}
-    </Document>
-    </>
+    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
+      <div style={{ position: 'relative', display: 'inline-block' }} ref={cameraRef}>
+      <Camera
+        isImageMirror={false}
+        onTakePhoto = {handleTakePhoto}
+        idealFacingMode={FACING_MODES.ENVIRONMENT}
+        idealResolution={{ width: window.innerWidth, height: window.innerHeight }}
+      />
+        <div style={{ position: 'absolute', bottom: 30, right: 30 }}>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
